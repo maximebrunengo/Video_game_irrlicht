35fa54ea23adba7fc41562c6aa4bf0d67539f3d5
Maylis Raffara <maylis.raffara@mastertp>
2015-11-27 10:01:18 +0100

    Ajout cube

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ int main()
 {
   // Le gestionnaire d'événements
   EventReceiver receiver;
+  std::vector<iv::ITexture*> textures;
 
   // Création de la fenêtre et du système de rendu.
   IrrlichtDevice *device = createDevice(iv::EDT_OPENGL,
@@ -21,9 +22,31 @@ int main()
   is::ISceneManager *smgr = device->getSceneManager();
   iv::IVideoDriver  *driver = device->getVideoDriver();
 
-  smgr->addCameraSceneNode(nullptr, ic::vector3df(0, 30, -40), ic::vector3df(0, 5, 0));
-  //is::ICameraSceneNode *camera = smgr->addCameraSceneNodeMaya();
-  //camera->setTarget(node->getPosition());
+  // Chargement du cube
+  is::IAnimatedMesh *mesh = smgr->getMesh("data/cube.3ds");
+
+  // Ajout du cube à la scène
+  is::IAnimatedMeshSceneNode *node;
+
+  //for (int i = 0 ; i < 3 ; i ++)
+    //for (int j = 0 ; j < 3; j ++)
+   // {
+	 // {
+	  node = smgr->addAnimatedMeshSceneNode(mesh);
+	  node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+	  node->setMD2Animation(irr::scene::EMAT_RUN);
+	  node->setPosition (core::vector3df(0,0,0));
+	  textures.push_back(driver->getTexture("data/TXsQk.png"));
+	  node->setMaterialTexture(0, textures[0]);
+	  receiver.set_node(node);
+	  receiver.set_textures(textures);
+	  //}
+    //}
+
+  //smgr->addCameraSceneNode(nullptr, ic::vector3df(0, 30, -40), ic::vector3df(0, 5, 0));
+  is::ICameraSceneNode *camera = smgr->addCameraSceneNodeMaya();
+  camera->setTarget(node->getPosition());
+  camera->setFOV(0.05f*M_PI);
 
   while(device->run())
   {
@@ -31,7 +54,6 @@ int main()
 
     // Dessin de la scène :
     smgr->drawAll();
-    //
     driver->endScene();
   }
   device->drop();
@@ -59,4 +81,5 @@ struct MyEventReceiver : IEventReceiver
     return false;
   }
 };
+
 
